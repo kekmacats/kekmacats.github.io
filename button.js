61d808bd160b1ea6b437abd0b7c6264c977a2c5c
kekmacats <nahdiscord0011@proton.me>
2024-06-18 20:51:47 +0200

    Add files via upload

--- button.js
+++ button.js
@@ -1,5 +1,6 @@
 var vid1audio = new Audio('vid1.mp4');
 var elem = document.documentElement;
+var input = document.getElementById("myInput");
 
 function openFullscreen() {
   if (elem.requestFullscreen) {
@@ -11,17 +12,29 @@ function openFullscreen() {
   }
 }
 
+window.addEventListener("beforeunload", (event) => {
+    event.preventDefault();
+	event.returnValue = "";
+});
+
 document.getElementById("toggleBtn").onclick = function() {
-        var vid = document.getElementById("vid");
-            vid.style.display = "block";
-            toggleBtn.style.display = "none";
-            ad.style.display = "none";
-            tube.style.display = "none";
-            txt.style.display = "none";
-            txt2.style.display = "none";
-			vid1audio.play();
-			openFullscreen()
-    };
+    var vid = document.getElementById("vid");
+        vid.style.display = "block";
+        toggleBtn.style.display = "none";
+        ad.style.display = "none";
+        tube.style.display = "none";
+        txt.style.display = "none";
+        txt2.style.display = "none";
+		vid1audio.play();
+		openFullscreen()
+};
+	
+elem.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("toggleBtn").click();
+  }
+});
 	
 document.getElementById("ad").onclick = function() {
         txt2.style.display = "block";
